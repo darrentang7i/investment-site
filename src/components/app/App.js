3b22c1c16d2392bc2b524c68107fd2ff0cafa002
darrentang7i <45007443+darrentang7i@users.noreply.github.com>
2020-08-12 14:49:04 -0400

    added new files

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomeButton from './Test.js'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <HomeButton></HomeButton>
     </div>
   );
 }
