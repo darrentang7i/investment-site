cabe19c80c28326f7627fbffd19bac98df70889c
jjwong <jj.wong@alumni.ubc.ca>
2020-08-13 15:39:24 -0700

    Populated Featured Learning Resources and Featured Investments

--- data.js
+++ data.js
@@ -43,7 +43,180 @@ const data = {
             content: "Take this interactive quiz to learn more about your risk tolerance and preferences to identify the appropriate investment strategy to reach your financial goals",
             link: "/"
         },
-    ]
+    ],
+    L1NoRiskResources: [
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L2NoRiskResources: [
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L3NoRiskResources: [
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "/"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L1SomeRiskResources: [
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L2SomeRiskResources: [
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L3SomeRiskResources: [
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L1RiskyResources: [
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 1 Beginner",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L2RiskyResources: [
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 2 Intermediate",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    L3RiskyResources: [
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        },
+        {
+            title: "Level 3 Advanced",
+            content: "Learn more about potential investment opportunities based on your risk preferences",
+            link: "https://www.scotiaitrade.com/en/direct-investing-and-online-trading/education.html"
+        }
+    ],
+    featuredInvestmentsA: {
+        title: "Bonds & GICs",
+        content: ["List of recommended investment options based on individual’s interests and goals specified on the quiz"],
+        links: []
+    },
+    featuredInvestmentsB: {
+        title: "Mutual Funds & ETFs",
+        content: [  "Scotia Diversified Monthly Income Fund",
+                    "Scotia Canadian Index Fund",
+                    "Scotia CanAm Index Fund"],
+        links: [    "https://www.scotiabank.com/content/scotiafunds/canada/scotiafunds/en/fund-overviews/balanced-funds/scotia-diversified-monthly-income-fund.html",
+                    "https://www.scotiabank.com/content/scotiafunds/canada/scotiafunds/en/fund-overviews/index-funds/scotia-canadian-index-fund.html",
+                    "https://www.scotiabank.com/content/scotiafunds/canada/scotiafunds/en/fund-overviews/index-funds/scotia-canam-index-fund.html"
+                    ]
+    },
+    featuredInvestmentsC: {
+        title: "Stocks",
+        content: ["List of recommended investment options based on individual’s interests and goals specified on the quiz"],
+        links: []
+    },
 }
 
 module.exports = data
