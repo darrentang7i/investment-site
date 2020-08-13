201b66b736c390c9e79eafcba42e8ccdb22e7ff7
watshever <louieweng@outlook.com>
2020-08-13 11:16:07 -0400

    finalized routes

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+// paths to routers
+var path = require('path')
+var indexRouter = require('./routes/index')
+var quizRouter = require('./routes/quiz')
+
+// connect app with routers
+app.use('/', indexRouter)
+app.use('/quiz', quizRouter)
+app.use(express.json())
+
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
