ca1f948a8e874a7f3c013542378794eb93f8b02d
darrentang7i <45007443+darrentang7i@users.noreply.github.com>
2020-08-13 12:12:37 -0400

    updated

--- src/components/quizPage/QuizPage.js
+++ src/components/quizPage/QuizPage.js
@@ -5,43 +5,50 @@ import Form from "react-bootstrap/Form";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-export function QuizPage() {
-    const [show, toggleShow] = useState(true);
+export class QuizPage extends React.Component {
+    // const [show, toggleShow] = useState(true);
 
+
+    onCheck(){
+        console.log("checkmark");
+    }
     // function startQuiz (){
 
     // }
 
-    return (
-        <div>
-            <div id="header">
-                
-                <Container fluid className="p-5" >
-                    <div id="quiz-title"> Interactive Quiz</div>
-                    <div>Take this interactive quiz to learn more about your risk tolerance and preferences to <br></br>identify the appropriate investment strategy to reach your financial goals</div>
-                </Container >
-            </div>
-            <div id="questions">
-                <Container>
-                    <Form>
-                        <Row className="pt-2">
-                            <div id="numbering">1</div>
-                            <div id="question-content">You are on a TV game show and have three options. Which one would you choose?</div>
-                        </Row>
+    render() {
+        return(
+            <div>
+                <div id="header">
+                    
+                    <Container fluid className="p-5" >
+                        <div id="quiz-title"> Interactive Quiz</div>
+                        <div>Take this interactive quiz to learn more about your risk tolerance and preferences to <br></br>identify the appropriate investment strategy to reach your financial goals</div>
+                    </Container >
+                </div>
+                <div id="questions">
+                    <Container>
+                        <Form>
+                            <Row className="pt-2">
+                                <div id="numbering">1</div>
+                                <div id="question-content">You are on a TV game show and have three options. Which one would you choose?</div>
+                            </Row>
 
-                        {['$1000 in cash', '50% chance at winning $5000', '10% chance of winning $500,000'].map((response) => (
-                            <div key={response} className="options mb-3">
-                            <Form.Check 
-                                custom
-                                type={'checkbox'}
-                                id={response}
-                                label={`${response}`}
-                            />
-                            </div>
-                        ))}
-                    </Form>
-                </Container>
+                            {['$1000 in cash', '50% chance at winning $5000', '10% chance of winning $500,000'].map((response) => (
+                                <div key={response} className="options mb-3">
+                                <Form.Check 
+                                    custom
+                                    type="checkbox"
+                                    id={response}
+                                    label={`${response}`}
+                                    onChange={() => {console.log("hello")}}
+                                />
+                                </div>
+                            ))}
+                        </Form>
+                    </Container>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
