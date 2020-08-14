1f08ffa0765429df5fdd99ecae6ed4f474c1eefb
jjwong783 <jjwong783@gmail.com>
2020-08-13 19:13:08 -0700

    Updated Button title and # of blogs

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -27,7 +27,7 @@ function App() {
           <Nav className="justify-content-around bg-transparent w-100">
             <Nav.Link as={Link} to="/quiz">Interactive Quiz</Nav.Link>
             <Nav.Link as={Link} to="/dashboard">My Dashboard</Nav.Link>
-            <Nav.Link as={Link} to="/blog">Expert Blogs</Nav.Link>
+            <Nav.Link as={Link} to="/blog">Blogs</Nav.Link>
             <Nav.Link href="https://www.scotiaitrade.com/en/direct-investing-and-online-trading.html">iTRADE</Nav.Link>
           </Nav>
         </Navbar.Collapse>
